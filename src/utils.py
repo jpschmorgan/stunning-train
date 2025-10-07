9e7bdb3377014b515befccfba08dc444097bcf73
jpschmorgan <jpschmorgan@users.noreply.github.com>
2025-10-07 21:49:10 +0300

    test: update error messages

--- src/utils.py
+++ src/utils.py
@@ -8,3 +8,14 @@ def process_data_7(data):
     if data:
         return data.upper()
     return None
+
+# Updated iteration 11
+def function_11():
+    """Helper function for feature 11"""
+    return True
+
+def process_data_11(data):
+    """Process data for iteration 11"""
+    if data:
+        return data.upper()
+    return None
